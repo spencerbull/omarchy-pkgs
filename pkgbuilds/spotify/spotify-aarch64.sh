615383b9ffbb6510f7f07db61eb4be0c264d0036
Spencer Bull <Spencer.Bull@Dell.com>
2026-08-25 04:38:18 -0500

    Harden AArch64 Spotify package

--- pkgbuilds/spotify/spotify-aarch64.sh
+++ pkgbuilds/spotify/spotify-aarch64.sh
@@ -3,14 +3,40 @@
 set -euo pipefail
 
 url="https://open.spotify.com/"
+chromium_args=()
+target=""
 
-if [[ ${1:-} == spotify:* ]]; then
-  path=${1#spotify:}
+while (($#)); do
+  case $1 in
+    --uri=spotify:*|-uri=spotify:*|--uri=https://open.spotify.com/*|-uri=https://open.spotify.com/*)
+      target=${1#*=}
+      shift
+      ;;
+    --uri|-uri)
+      if [[ ${2:-} == spotify:* || ${2:-} == https://open.spotify.com/* ]]; then
+        target=$2
+        shift 2
+      else
+        chromium_args+=("$1")
+        shift
+      fi
+      ;;
+    spotify:*|https://open.spotify.com/*)
+      target=$1
+      shift
+      ;;
+    *)
+      chromium_args+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [[ $target == spotify:* ]]; then
+  path=${target#spotify:}
   url="https://open.spotify.com/${path//://}"
-  shift
-elif [[ ${1:-} == https://open.spotify.com/* ]]; then
-  url=$1
-  shift
+elif [[ $target == https://open.spotify.com/* ]]; then
+  url=$target
 fi
 
-exec chromium --app="$url" "$@"
+exec chromium --app="$url" "${chromium_args[@]}"
